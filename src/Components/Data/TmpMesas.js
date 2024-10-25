4a328d611b6d5bb9f09c3c7d80fd66159e44a4bc
Emmanuel Rivera <U22222056@utp.edu.pe>
2024-10-25 13:23:56 -0500

    Creación de archivo para con información inicial de las mesas.

---
+++ src/Components/Data/TmpMesas.js
@@ -0,0 +1,67 @@
+const MESAS = [
+    {
+        estado: "Libre",
+        numMesa: "1"
+    },
+    {
+        estado: "Libre",
+        numMesa: "2"
+    },
+    {
+        estado: "Ocupado",
+        numMesa: "3"
+    },
+    {       
+        estado: "Reservado",
+        numMesa: "4"
+    },
+    {
+        estado: "Ocupado",
+        numMesa: "5"
+    },
+    {
+        estado: "Ocupado",
+        numMesa: "6"
+    },
+    {
+        estado: "Libre",
+        numMesa: "7"
+    },
+    {
+        estado: "Libre",
+        numMesa: "8"
+    },
+    {
+        estado: "Reservado",
+        numMesa: "9"
+    },
+    {
+        estado: "Reservado",
+        numMesa: "10"
+    },
+    {
+        estado: "Libre",
+        numMesa: "11"
+    },
+    {
+        estado: "Libre",
+        numMesa: "12"
+    },
+    {
+        estado: "Libre",
+        numMesa: "13"
+    },
+    {
+        estado: "Libre",
+        numMesa: "14"
+    },
+    {
+        estado: "Libre",
+        numMesa: "15"
+    },
+    {
+        estado: "Libre",
+        numMesa: "16"
+    }
+]
+export default MESAS
